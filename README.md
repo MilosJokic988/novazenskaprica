1fb985dbf96a1fe347809d8e5fbde0e703caeba5
NothicDesign <jokicmilos152@gmail.com>
2025-07-31 09:35:11 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-# Nova ženska priča
+# React + Vite
 
-Ovo je zvanični sajt “Nova ženska priča” – moderna i profesionalna veb prezentacija namenjena predstavljanju usluga i informacija za žensku publiku.
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
----
+Currently, two official plugins are available:
 
-## Tehnologije korišćene
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-- React  
-- Vite  
-- Vercel (za hosting i deployment)  
-- CSS (običan ili po potrebi)
+## Expanding the ESLint configuration
 
----
-
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
--- api/sitemap.js
+++ api/sitemap.js
@@ -1,12 +1,31 @@
 export default function handler(req, res) {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url><loc>https://novazenskaprica.rs/</loc><priority>1.00</priority></url>
-    <url><loc>https://novazenskaprica.rs/o-meni</loc><priority>0.80</priority></url>
-    <url><loc>https://novazenskaprica.rs/usluge</loc><priority>0.80</priority></url>
-    <url><loc>https://novazenskaprica.rs/kontakt</loc><priority>0.80</priority></url>
-  </urlset>`;
-
-  res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(sitemap);
+  res.setHeader("Content-Type", "application/xml");
+  res.write(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://novazenskaprica.rs/</loc>
+    <priority>1.00</priority>
+  </url>
+  <url>
+    <loc>https://novazenskaprica.rs/usluge</loc>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>https://novazenskaprica.rs/prodavnica</loc>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>https://novazenskaprica.rs/galerija</loc>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>https://novazenskaprica.rs/o-nama</loc>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>https://novazenskaprica.rs/kontakt</loc>
+    <priority>0.80</priority>
+  </url>
+</urlset>`);
+  res.end();
 }
